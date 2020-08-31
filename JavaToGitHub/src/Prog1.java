fed2c99eecf7ffc9723273aa415c52dc7c87c473
uma_s <uma_s@DESKTOP-SKA6I02>
2020-08-30 19:38:11 -0700

    added 4 & 5 line

---
+++ JavaToGitHub/src/Prog1.java
@@ -0,0 +1,12 @@
+
+public class Prog1 {
+
+	public static void main(String[] args) {
+		System.out.println("Try the send Java code to Git");
+		System.out.println("Second Line");
+		System.out.println("Third Line");
+		System.out.println("4 Line");
+		System.out.println("5 Line");
+	}
+
+}
